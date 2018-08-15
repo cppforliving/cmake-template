66a1ad7c716eb16a3ca82fb0ce4889c961babff1
cppforliving <cppforliving@gmail.com>
2018-08-15 18:02:01 +0200

    googletest-download module cleanup

--- CMake/googletest-download.cmake
+++ CMake/googletest-download.cmake
@@ -1,7 +1,7 @@
 # Source https://github.com/google/googletest/blob/master/googletest/README.md#incorporating-into-an-existing-cmake-project
 # Download and unpack googletest at configure time
 
-configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in
+configure_file(${CMAKE_CURRENT_LIST_DIR}/googletest-download/CMakeLists.txt.in
                ${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
@@ -27,11 +27,3 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 add_subdirectory(${CMAKE_BINARY_DIR}/googletest-src
                  ${CMAKE_BINARY_DIR}/googletest-build
                  EXCLUDE_FROM_ALL)
-
-# The gtest/gtest_main targets carry header search path
-# dependencies automatically when using CMake 2.8.11 or
-# later. Otherwise we have to add them here ourselves.
-if(CMAKE_VERSION VERSION_LESS 2.8.11)
-  include_directories("${gtest_SOURCE_DIR}/include"
-                      "${gmock_SOURCE_DIR}/include")
-endif()
