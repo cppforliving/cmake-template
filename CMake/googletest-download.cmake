40f51d062628ce5c659c0a45bd7235cd3dee88a9
cppforliving <cppforliving@gmail.com>
2018-08-15 18:45:03 +0200

    Handling spaces in project path

--- CMake/googletest-download.cmake
+++ CMake/googletest-download.cmake
@@ -1,19 +1,19 @@
 # Source https://github.com/google/googletest/blob/master/googletest/README.md#incorporating-into-an-existing-cmake-project
 # Download and unpack googletest at configure time
 
-configure_file(${CMAKE_CURRENT_LIST_DIR}/googletest-download/CMakeLists.txt.in
-               ${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/googletest-download/CMakeLists.txt.in"
+               "${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt")
 
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
   RESULT_VARIABLE result
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download)
+  WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download")
 if(result)
   message(FATAL_ERROR "CMake step for googletest failed: ${result}")
 endif()
 
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
   RESULT_VARIABLE result
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download)
+  WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download")
 if(result)
   message(FATAL_ERROR "Build step for googletest failed: ${result}")
 endif()
@@ -24,6 +24,6 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 # Add googletest directly to our build. This defines
 # the gtest and gtest_main targets.
-add_subdirectory(${CMAKE_BINARY_DIR}/googletest-src
-                 ${CMAKE_BINARY_DIR}/googletest-build
+add_subdirectory("${CMAKE_BINARY_DIR}/googletest-src"
+                 "${CMAKE_BINARY_DIR}/googletest-build"
                  EXCLUDE_FROM_ALL)
