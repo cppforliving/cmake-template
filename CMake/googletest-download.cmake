92ac97d87faa5e9220b1c0a73532cceff69ee589
cppforliving <cppforliving@gmail.com>
2018-08-15 19:31:49 +0200

    cmake path with spaces supported

--- CMake/googletest-download.cmake
+++ CMake/googletest-download.cmake
@@ -4,14 +4,14 @@
 configure_file("${CMAKE_CURRENT_LIST_DIR}/googletest-download/CMakeLists.txt.in"
                "${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt")
 
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
     RESULT_VARIABLE result
     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download")
 if(result)
     message(FATAL_ERROR "CMake step for googletest failed: ${result}")
 endif()
 
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
+execute_process(COMMAND "${CMAKE_COMMAND}" --build .
     RESULT_VARIABLE result
     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download")
 if(result)
