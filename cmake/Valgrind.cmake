21f913765059067b47487e25ae605c6c8444a999
cppforliving <cppforliving@gmail.com>
2019-02-14 12:50:06 +0100

    get rid of test-reports subdirectory

--- cmake/Valgrind.cmake
+++ cmake/Valgrind.cmake
@@ -10,23 +10,23 @@ if(valgrind_tool STREQUAL memcheck)
         --error-exitcode=1 \
         --read-inline-info=yes \
         --xtree-memory=full \
-        --xtree-memory-file=test-reports/memcheck-xtmemory.kcg \
+        --xtree-memory-file=memcheck-xtmemory.kcg \
         --child-silent-after-fork=yes \
         --xml=yes \
-        --xml-file=test-reports/valgrind.xml")
+        --xml-file=valgrind.xml")
 # cachegrind
 elseif(valgrind_tool STREQUAL callgrind)
     string(APPEND VALGRIND_COMMAND_OPTIONS " \
         --dump-instr=yes \
         --collect-jumps=yes \
-        --callgrind-out-file=test-reports/callgrind.out")
-# helgrind --xtree-memory=full --xtree-memory-file=test-reports/helgrind-xtmemory.kcg
+        --callgrind-out-file=callgrind.out")
+# helgrind --xtree-memory=full --xtree-memory-file=helgrind-xtmemory.kcg
 # drd
 elseif(valgrind_tool STREQUAL massif)
     string(APPEND VALGRIND_COMMAND_OPTIONS " \
         --xtree-memory=full \
-        --xtree-memory-file=test-reports/massif-xtmemory.kcg \
-        --massif-out-file=test-reports/massif.out")
+        --xtree-memory-file=massif-xtmemory.kcg \
+        --massif-out-file=massif.out")
 else()
     message(FATAL_ERROR "valgrind_tool=${valgrind_tool} not supported yet")
 endif()
--- cmake/gcovr.cmake
+++ cmake/gcovr.cmake
@@ -4,12 +4,11 @@ if(GCOVR_COMMAND)
     function(add_gcovr_command coverage_target)
         if(build_coverage STREQUAL html OR build_coverage STREQUAL xml)
             add_custom_command(TARGET ${coverage_target} POST_BUILD
-                COMMAND ${CMAKE_COMMAND} -E make_directory test-reports
                 COMMAND ${GCOVR_COMMAND} --${build_coverage}
                     $<$<STREQUAL:${build_coverage},html>:--html-details>
                     -r "${PROJECT_SOURCE_DIR}/src"
                     --object-directory="${PROJECT_BINARY_DIR}"
-                    -o "${PROJECT_BINARY_DIR}/test-reports/coverage.${build_coverage}"
+                    -o "${PROJECT_BINARY_DIR}/coverage.${build_coverage}"
                 COMMENT "Generating gcovr ${build_coverage} reports")
         elseif(NOT build_coverage STREQUAL "")
             message(FATAL_ERROR "build_coverage=${build_coverage} not supported yet")
