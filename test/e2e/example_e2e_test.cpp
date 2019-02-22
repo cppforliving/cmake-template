c623e7045e977c0e44a7e248f33844a41f3c5240
cppforliving <cppforliving@gmail.com>
2019-02-22 16:18:30 +0100

    travis use MSVC 15, remove useless test

--- test/e2e/example_e2e_test.cpp
+++ test/e2e/example_e2e_test.cpp
@@ -1,18 +1,8 @@
-#include <algorithm>
 #include <catch2/catch.hpp>
-#include <memory>
-#include <string>
 #include "example/example.hpp"
 
 TEST_CASE("example_e2e_test.all") {
     auto x = example::newInt123();
     REQUIRE(*x == 123);
     delete x;
-}
-
-TEST_CASE("example_e2e_test.efence") {
-    const auto a = std::make_unique<char[]>(sizeof("Alpha"));
-    const auto b = std::make_unique<char[]>(sizeof("Bravo"));
-    const auto c = std::string("Charlie");
-    std::copy(c.begin(), c.cend(), a.get());
 }
