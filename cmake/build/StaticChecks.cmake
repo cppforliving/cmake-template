eaeb540e59c900c751e33dd10bec011e4ea633d0
cppforliving <cppforliving@gmail.com>
2019-02-24 18:23:39 +0100

    remove superfluous qouting

--- cmake/build/StaticChecks.cmake
+++ cmake/build/StaticChecks.cmake
@@ -1,7 +1,7 @@
 set(${PROJECT_NAME}_check "" CACHE STRING
     "Static code analysis tools: clang-tidy, cppcheck, cpplint, iwyu, lwyu.")
 
-if("${${PROJECT_NAME}_check}" STREQUAL clang-tidy)
+if(${${PROJECT_NAME}_check} STREQUAL clang-tidy)
     find_program(CLANG_TIDY_COMMAND clang-tidy)
     mark_as_advanced(CLANG_TIDY_COMMAND)
     set(CMAKE_C_CLANG_TIDY ${CLANG_TIDY_COMMAND}
@@ -9,21 +9,21 @@ if("${${PROJECT_NAME}_check}" STREQUAL clang-tidy)
         -warnings-as-errors=*
     )
     set(CMAKE_CXX_CLANG_TIDY ${CMAKE_C_CLANG_TIDY})
-elseif("${${PROJECT_NAME}_check}" STREQUAL cppcheck)
+elseif(${${PROJECT_NAME}_check} STREQUAL cppcheck)
     find_program(CPPCHECK_COMMAND cppcheck)
     mark_as_advanced(CPPCHECK_COMMAND)
     set(CMAKE_C_CPPCHECK ${CPPCHECK_COMMAND}
         --enable=all
     )
     set(CMAKE_CXX_CPPCHECK ${CMAKE_C_CPPCHECK})
-elseif("${${PROJECT_NAME}_check}" STREQUAL cpplint)
+elseif(${${PROJECT_NAME}_check} STREQUAL cpplint)
     find_program(CPPLINT_COMMAND cpplint)
     mark_as_advanced(CPPLINT_COMMAND)
     set(CMAKE_C_CPPLINT ${CPPLINT_COMMAND}
         --filter=-legal/copyright
     )
     set(CMAKE_CXX_CPPLINT ${CMAKE_C_CPPLINT})
-elseif("${${PROJECT_NAME}_check}" STREQUAL iwyu)
+elseif(${${PROJECT_NAME}_check} STREQUAL iwyu)
     find_program(IWYU_COMMAND iwyu)
     mark_as_advanced(IWYU_COMMAND)
     set(CMAKE_C_INCLUDE_WHAT_YOU_USE ${IWYU_COMMAND}
@@ -33,8 +33,8 @@ elseif("${${PROJECT_NAME}_check}" STREQUAL iwyu)
     if(NOT MSVC)
         list(APPEND CMAKE_CXX_INCLUDE_WHAT_YOU_USE -nostdinc++)
     endif()
-elseif("${${PROJECT_NAME}_check}" STREQUAL lwyu)
+elseif(${${PROJECT_NAME}_check} STREQUAL lwyu)
     set(CMAKE_LINK_WHAT_YOU_USE ON)
-elseif(NOT "${${PROJECT_NAME}_check}" STREQUAL "")
+elseif(NOT ${${PROJECT_NAME}_check} STREQUAL "")
     message(FATAL_ERROR "${PROJECT_NAME}_check=${${PROJECT_NAME}_check} not supported yet")
 endif()
