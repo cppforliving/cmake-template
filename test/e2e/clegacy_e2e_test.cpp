c0c5401313f3c7173ba9cd1d9da56a578f53b2a5
cppforliving <cppforliving@gmail.com>
2019-02-24 22:58:44 +0100

    free memory allocated by malloc

--- test/e2e/clegacy_e2e_test.cpp
+++ test/e2e/clegacy_e2e_test.cpp
@@ -1,8 +1,9 @@
+#include <stdlib.h>
 #include <catch2/catch.hpp>
 #include "clegacy/clegacy.h"
 
 TEST_CASE("clegacy_e2e_test.all") {
     auto x = clegacy_newInt123();
     REQUIRE(*x == 123);
-    delete x;
+    free(x);
 }
