5d5b80e268d705391fff7306b0a3e7504f50c1cd
cppforliving <cppforliving@gmail.com>
2019-02-24 18:37:12 +0100

    fix gcovr config and improve static checks

--- cmake/build/StaticChecks.cmake
+++ cmake/build/StaticChecks.cmake
@@ -1,32 +1,32 @@
-set(${PROJECT_NAME}_check "" CACHE STRING
-    "Static code analysis tools: clang-tidy, cppcheck, cpplint, iwyu, lwyu.")
+set(${PROJECT_NAME}_check none CACHE STRING
+    "Static code analysis tools: clang-tidy, cppcheck, cpplint, iwyu, lwyu, none (default).")
 
 if(${${PROJECT_NAME}_check} STREQUAL clang-tidy)
-    find_program(CLANG_TIDY_COMMAND clang-tidy)
-    mark_as_advanced(CLANG_TIDY_COMMAND)
-    set(CMAKE_C_CLANG_TIDY ${CLANG_TIDY_COMMAND}
+    find_program(clang_tidy_command clang-tidy)
+    mark_as_advanced(clang_tidy_command)
+    set(CMAKE_C_CLANG_TIDY ${clang_tidy_command}
         -checks=*,-cert-err58-cpp,-cppcoreguidelines-owning-memory,-cppcoreguidelines-special-member-functions,-fuchsia-default-arguments,-hicpp-special-member-functions,-llvm-header-guard,-llvm-include-order,-clang-diagnostic-unused-command-line-argument,-clang-diagnostic-ignored-optimization-argument
         -warnings-as-errors=*
     )
     set(CMAKE_CXX_CLANG_TIDY ${CMAKE_C_CLANG_TIDY})
 elseif(${${PROJECT_NAME}_check} STREQUAL cppcheck)
-    find_program(CPPCHECK_COMMAND cppcheck)
-    mark_as_advanced(CPPCHECK_COMMAND)
-    set(CMAKE_C_CPPCHECK ${CPPCHECK_COMMAND}
+    find_program(cppcheck_command cppcheck)
+    mark_as_advanced(cppcheck_command)
+    set(CMAKE_C_CPPCHECK ${cppcheck_command}
         --enable=all
     )
     set(CMAKE_CXX_CPPCHECK ${CMAKE_C_CPPCHECK})
 elseif(${${PROJECT_NAME}_check} STREQUAL cpplint)
-    find_program(CPPLINT_COMMAND cpplint)
-    mark_as_advanced(CPPLINT_COMMAND)
-    set(CMAKE_C_CPPLINT ${CPPLINT_COMMAND}
+    find_program(cpplint_command cpplint)
+    mark_as_advanced(cpplint_command)
+    set(CMAKE_C_CPPLINT ${cpplint_command}
         --filter=-legal/copyright
     )
     set(CMAKE_CXX_CPPLINT ${CMAKE_C_CPPLINT})
 elseif(${${PROJECT_NAME}_check} STREQUAL iwyu)
-    find_program(IWYU_COMMAND iwyu)
-    mark_as_advanced(IWYU_COMMAND)
-    set(CMAKE_C_INCLUDE_WHAT_YOU_USE ${IWYU_COMMAND}
+    find_program(iwyu_command iwyu)
+    mark_as_advanced(iwyu_command)
+    set(CMAKE_C_INCLUDE_WHAT_YOU_USE ${iwyu_command}
         -Xiwyu --mapping_file=${PROJECT_SOURCE_DIR}/iwyu.imp
     )
     set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE ${CMAKE_C_INCLUDE_WHAT_YOU_USE})
@@ -35,6 +35,6 @@ elseif(${${PROJECT_NAME}_check} STREQUAL iwyu)
     endif()
 elseif(${${PROJECT_NAME}_check} STREQUAL lwyu)
     set(CMAKE_LINK_WHAT_YOU_USE ON)
-elseif(NOT ${${PROJECT_NAME}_check} STREQUAL "")
+elseif(NOT ${${PROJECT_NAME}_check} STREQUAL none)
     message(FATAL_ERROR "${PROJECT_NAME}_check=${${PROJECT_NAME}_check} not supported yet")
 endif()
--- cmake/gcovr.cmake
+++ cmake/gcovr.cmake
@@ -2,16 +2,16 @@ find_program(gcovr_command gcovr)
 mark_as_advanced(gcovr_command)
 if(gcovr_command)
     function(add_gcovr_command coverage_target)
-        if(build_coverage STREQUAL html OR build_coverage STREQUAL xml)
+        if(${PROJECT_NAME}_coverage STREQUAL html OR ${PROJECT_NAME}_coverage STREQUAL xml)
             add_custom_command(TARGET ${coverage_target} POST_BUILD
-                COMMAND ${gcovr_command} --${build_coverage}
-                    $<$<STREQUAL:${build_coverage},html>:--html-details>
+                COMMAND ${gcovr_command} --${${PROJECT_NAME}_coverage}
+                    $<$<STREQUAL:${${PROJECT_NAME}_coverage},html>:--html-details>
                     -r "${PROJECT_SOURCE_DIR}/src"
                     --object-directory="${PROJECT_BINARY_DIR}"
-                    -o "${PROJECT_BINARY_DIR}/coverage.${build_coverage}"
-                COMMENT "Generating gcovr ${build_coverage} reports")
-        elseif(NOT build_coverage STREQUAL "")
-            message(FATAL_ERROR "build_coverage=${build_coverage} not supported yet")
+                    -o "${PROJECT_BINARY_DIR}/coverage.${${PROJECT_NAME}_coverage}"
+                COMMENT "Generating gcovr ${${PROJECT_NAME}_coverage} reports")
+        elseif(NOT ${PROJECT_NAME}_coverage STREQUAL "")
+            message(FATAL_ERROR "${PROJECT_NAME}_coverage=${${PROJECT_NAME}_coverage} not supported yet")
         endif()
     endfunction()
     add_gcovr_command(ExperimentalCoverage)
