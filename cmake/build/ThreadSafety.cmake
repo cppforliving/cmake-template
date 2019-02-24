12df36162561f67d219adade91cf145d495084ab
cppforliving <cppforliving@gmail.com>
2019-02-24 18:02:06 +0100

    remove unused ThreadSafety cmake module

--- cmake/build/ThreadSafety.cmake
+++
@@ -1,9 +0,0 @@
-option(build_thread_safety "Build for thread safety analysis." OFF)
-
-if(build_thread_safety)
-    if(${CMAKE_CXX_COMPILER_ID} MATCHES Clang)
-        add_compile_options(-Wthread-safety)
-    else()
-        message(FATAL_ERROR "build_thread_safety not supported yet for ${CMAKE_CXX_COMPILER_ID}")
-    endif()
-endif()
