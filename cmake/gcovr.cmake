9babf1b7352d413aaa6a83dfd1784dc81a3219cd
cppforliving <cppforliving@gmail.com>
2019-02-26 01:42:04 +0100

    gcovr.cmake refactoring

--- cmake/gcovr.cmake
+++ cmake/gcovr.cmake
@@ -1,20 +1,17 @@
 find_program(gcovr_command gcovr)
 mark_as_advanced(gcovr_command)
 
-function(add_gcovr_command coverage_target)
-    if(${PROJECT_NAME}_coverage STREQUAL html OR ${PROJECT_NAME}_coverage STREQUAL xml)
+if(${PROJECT_NAME}_coverage STREQUAL html OR ${PROJECT_NAME}_coverage STREQUAL xml)
+    foreach(coverage_target ExperimentalCoverage ContinuousCoverage NightlyCoverage)
         add_custom_command(TARGET ${coverage_target} POST_BUILD
-            COMMAND ${gcovr_command} --${${PROJECT_NAME}_coverage}
-                $<$<STREQUAL:${${PROJECT_NAME}_coverage},html>:--html-details>
+            COMMAND ${gcovr_command}
                 -r "${PROJECT_SOURCE_DIR}/src"
                 --object-directory="${PROJECT_BINARY_DIR}"
+                --${${PROJECT_NAME}_coverage}
+                $<$<STREQUAL:${${PROJECT_NAME}_coverage},html>:--html-details>
                 -o "${PROJECT_BINARY_DIR}/coverage.${${PROJECT_NAME}_coverage}"
             COMMENT "Generating gcovr ${${PROJECT_NAME}_coverage} reports")
-    elseif(NOT ${PROJECT_NAME}_coverage STREQUAL "")
-        message(FATAL_ERROR "${PROJECT_NAME}_coverage=${${PROJECT_NAME}_coverage} not supported yet")
-    endif()
-endfunction()
-
-add_gcovr_command(ExperimentalCoverage)
-add_gcovr_command(ContinuousCoverage)
-add_gcovr_command(NightlyCoverage)
+    endforeach()
+elseif(NOT ${PROJECT_NAME}_coverage STREQUAL "")
+    message(FATAL_ERROR "${PROJECT_NAME}_coverage=${${PROJECT_NAME}_coverage} not supported yet")
+endif()
