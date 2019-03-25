a39d46ef36db2a5b21f1cdeef546f94ccd9cd51e
cppforliving <cppforliving@gmail.com>
2019-03-25 22:27:41 +0100

    Lockable added

---
+++ src/example/lockable.hpp
@@ -0,0 +1,54 @@
+#ifndef EXAMPLE_LOCKABLE_HPP
+#define EXAMPLE_LOCKABLE_HPP
+
+#include <mutex>
+#include <utility>
+
+namespace example {
+
+template <typename T, typename M = std::mutex>
+class Lockable {
+public:
+    class Lock {
+    public:
+        explicit Lock(Lockable& lockable) : m_lockable{lockable} { m_lockable.m_mutex.lock(); }
+        Lock(Lockable& lockable, std::adopt_lock_t) noexcept : m_lockable(lockable) {}
+        ~Lock() { m_lockable.m_mutex.unlock(); }
+
+        Lock(Lock const&) = delete;
+        void operator=(Lock const&) = delete;
+
+        T* operator->() noexcept { return &m_lockable.m_data; }
+        T const* operator->() const noexcept { return &m_lockable.m_data; }
+
+        T& operator*() noexcept { return m_lockable.m_data; }
+        T const& operator*() const noexcept { return m_lockable.m_data; }
+
+    private:
+        Lockable& m_lockable;
+    };
+
+    Lockable() = default;
+    ~Lockable() = default;
+
+    template <typename... Args>
+    explicit Lockable(Args&&... args) : m_data{std::forward<Args>(args)...} {}
+
+    Lockable(Lockable const&) = delete;
+    void operator=(Lockable const&) = delete;
+
+    void lock() { m_mutex.lock(); }
+    bool try_lock() noexcept { return m_mutex.try_lock(); }
+    void unlock() { m_mutex.unlock(); }
+
+private:
+    T m_data{};
+    M m_mutex{};
+};
+
+template <typename T>
+using RecursiveLockable = Lockable<T, std::recursive_mutex>;
+
+}  // namespace example
+
+#endif  // EXAMPLE_LOCKABLE_HPP
---
+++ src/example/lockable_test.cpp
@@ -0,0 +1,21 @@
+#include "lockable.hpp"
+
+#include <gtest/gtest.h>
+#include <mutex>
+#include <string>
+
+namespace example {
+namespace test {
+
+TEST(Lockable, shouldLock) {
+    Lockable<std::string> s1{"asd"};
+    Lockable<std::string> s2{"qwe"};
+    std::lock(s1, s2);
+    Lockable<std::string>::Lock l1{s1, std::adopt_lock};
+    Lockable<std::string>::Lock l2{s2, std::adopt_lock};
+    std::swap(*l1, *l2);
+    EXPECT_EQ(6, l1->size() + l2->size());
+}
+
+}  // namespace test
+}  // namespace example
