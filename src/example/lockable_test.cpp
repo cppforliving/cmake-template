7aeb640cb3dd2bc3d595151ce5d4d6c1028f39a0
cppforliving <cppforliving@gmail.com>
2019-04-10 23:51:55 +0200

    fix build on windows

--- src/example/lockable_test.cpp
+++ src/example/lockable_test.cpp
@@ -14,7 +14,7 @@ TEST(Lockable, shouldLock) {
     Lockable<std::string>::Lock l1{s1, std::adopt_lock};
     Lockable<std::string>::Lock l2{s2, std::adopt_lock};
     std::swap(*l1, *l2);
-    EXPECT_EQ(6, l1->size() + l2->size());
+    EXPECT_EQ(6u, l1->size() + l2->size());
 }
 
 }  // namespace test
