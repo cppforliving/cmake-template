f1091fcebb7b31b16a4c156caf366598ced2cdb1
cppforliving <cppforliving@gmail.com>
2019-04-16 10:22:09 +0200

    lockable test compilation fix

--- src/example/lockable_test.cpp
+++ src/example/lockable_test.cpp
@@ -11,28 +11,28 @@ using namespace example;
 
 TEST(Lockable, lockAndUnlock) {
     Lockable<std::string> s1{"asd"};
-    SUCCESS() << "lockable created";
+    SUCCEED() << "lockable created";
     s1.lock();
-    SUCCESS() << "lockable locked";
+    SUCCEED() << "lockable locked";
     Lockable<std::string>::Lock l1{s1, std::adopt_lock};
-    SUCCESS() << "lockable adopted";
+    SUCCEED() << "lockable adopted";
     EXPECT_EQ("asd", *l1);
-    SUCCESS() << "lockable examined";
+    SUCCEED() << "lockable examined";
 }
 
 TEST(Lockable, lockAndSwap) {
     Lockable<std::string> s1{"asd"};
     Lockable<std::string> s2{"qwe"};
-    SUCCESS() << "lockables created";
+    SUCCEED() << "lockables created";
     std::lock(s1, s2);
-    SUCCESS() << "lockables locked";
+    SUCCEED() << "lockables locked";
     Lockable<std::string>::Lock l1{s1, std::adopt_lock};
     Lockable<std::string>::Lock l2{s2, std::adopt_lock};
-    SUCCESS() << "lockables adopted";
+    SUCCEED() << "lockables adopted";
     std::swap(*l1, *l2);
-    SUCCESS() << "lockables swapped";
+    SUCCEED() << "lockables swapped";
     EXPECT_EQ(6u, l1->size() + l2->size());
-    SUCCESS() << "lockables examined";
+    SUCCEED() << "lockables examined";
 }
 
 }  // namespace
