3e5c7b1af1bdf020cd5f56c9d2d3645955b74f5f
cppforliving <cppforliving@gmail.com>
2019-05-03 14:46:58 +0200

    provide generic add_custom_test function

--- cmake/ProjectHelpers.cmake
+++ cmake/ProjectHelpers.cmake
@@ -18,10 +18,10 @@ function(debug_dynamic_dependencies target_name)
     endif()
 endfunction()
 
-function(add_gtest_test test_name)
+function(add_custom_test test_name)
     set(options)
     set(one_value_args)
-    set(multi_value_args SOURCES DEPENDS)
+    set(multi_value_args SOURCES DEPENDS TESTARGS)
     cmake_parse_arguments(${test_name} "${options}"
         "${one_value_args}" "${multi_value_args}" ${ARGN})
 
@@ -32,49 +32,10 @@ function(add_gtest_test test_name)
     )
     target_link_libraries(${test_name}
       PRIVATE
-        GTest::Main
-        GTest::GTest
-        ${${test_name}_DEPENDS}
-    )
-    target_compile_definitions(${test_name}
-      PRIVATE
-        GTEST_LINKED_AS_SHARED_LIBRARY=1
-    )
-    if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-        target_compile_options(${test_name}
-          PRIVATE
-            -Wno-global-constructors
-            -Wno-gnu-zero-variadic-macro-arguments
-            -Wno-zero-as-null-pointer-constant
-        )
-    endif()
-    debug_dynamic_dependencies(${test_name})
-    add_test(NAME ${test_name} COMMAND ${test_name}
-        --gtest_color=yes
-        --gtest_output=xml:${test_name}.xml
-    )
-endfunction()
-
-function(add_catch_test test_name)
-    set(options)
-    set(one_value_args)
-    set(multi_value_args SOURCES DEPENDS)
-    cmake_parse_arguments(${test_name} "${options}"
-        "${one_value_args}" "${multi_value_args}" ${ARGN})
-
-    add_executable(${test_name})
-    target_sources(${test_name}
-      PRIVATE
-        ${${test_name}_SOURCES}
-    )
-    target_link_libraries(${test_name}
-      PRIVATE
-        catch_main
-        Catch2::Catch2
         ${${test_name}_DEPENDS}
     )
     debug_dynamic_dependencies(${test_name})
     add_test(NAME ${test_name} COMMAND ${test_name}
-        --use-colour=yes
+        ${${test_name}_TESTARGS}
     )
 endfunction()
