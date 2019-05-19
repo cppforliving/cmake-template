d427eecd1d3e4c598feec3a6a1e4994e794793df
cppforliving <cppforliving@gmail.com>
2019-05-20 01:19:16 +0200

    improve FindGSL

--- cmake/FindGSL.cmake
+++ cmake/FindGSL.cmake
@@ -13,13 +13,11 @@ find_package_handle_standard_args(GSL
     GSL_VERSION
 )
 
-if(GSL_INCLUDE_DIRS)
-    if(NOT TARGET GSL::GSL)
-        add_library(GSL::GSL INTERFACE IMPORTED)
-        set_target_properties(
-            GSL::GSL
-          PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${GSL_INCLUDE_DIRS}"
-        )
-    endif()
+if(GSL_FOUND AND NOT TARGET GSL::GSL)
+    add_library(GSL::GSL INTERFACE IMPORTED)
+    set_target_properties(
+        GSL::GSL
+      PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${GSL_INCLUDE_DIRS}"
+    )
 endif()
