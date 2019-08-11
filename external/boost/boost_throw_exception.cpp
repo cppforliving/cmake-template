e7be627e8ac1c12fa8cd663e631b2905fd5b9bdb
cppforliving <cppforliving@gmail.com>
2019-08-11 23:19:23 +0200

    boost_throw_exception cleanup

--- external/boost/boost_throw_exception.cpp
+++ external/boost/boost_throw_exception.cpp
@@ -3,12 +3,9 @@
 
 #include <boost/throw_exception.hpp>
 
-#include "boost_throw_exception_export.h"
-
 namespace boost {
 
-BOOST_THROW_EXCEPTION_EXPORT void throw_exception(
-    std::exception const& /*unused*/) {
+void throw_exception(std::exception const& /*unused*/) {
     std::terminate();
 }
 
