f19b1b9200e1a4566154c6b13fa8b8418fef527a
cppforliving <cppforliving@gmail.com>
2019-08-16 01:00:39 +0200

    improve coverage report generation

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -18,14 +18,19 @@ if(${PROJECT_NAME}_coverage IN_LIST gcovr_coverage_types)
     find_program(gcovr_command gcovr)
     mark_as_advanced(gcovr_command)
     foreach(coverage_target IN LISTS coverage_targets)
+        set(gcovr_options
+            --root "${PROJECT_SOURCE_DIR}"
+            --exclude-directories "external"
+            --exclude-directories "tests"
+            --exclude ".*_test.cpp"
+            --object-directory "${PROJECT_BINARY_DIR}"
+        )
         add_custom_command(TARGET ${coverage_target} POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E make_directory coverage
-            COMMAND ${gcovr_command}
-                --root "${PROJECT_SOURCE_DIR}"
-                --exclude-directories "external"
-                --exclude-directories "tests"
-                --exclude ".*_test.cpp"
-                --object-directory "${PROJECT_BINARY_DIR}"
+            COMMAND ${gcovr_command} ${gcovr_options}
+            COMMAND ${gcovr_command} ${gcovr_options}
+                --branches
+            COMMAND ${gcovr_command} ${gcovr_options}
                 $<$<STREQUAL:${${PROJECT_NAME}_coverage},xml>:--xml>
                 $<$<STREQUAL:${${PROJECT_NAME}_coverage},html>:--html-details>
                 $<$<STREQUAL:${${PROJECT_NAME}_coverage},html>:--html-title>
@@ -39,13 +44,34 @@ elseif(${PROJECT_NAME}_coverage IN_LIST lcov_coverage_types)
     find_program(genhtml_command genhtml)
     mark_as_advanced(lcov_command genhtml_command)
     foreach(coverage_target IN LISTS coverage_targets)
-        set(lcov_options -q -o coverage.info --rc lcov_branch_coverage=1)
+        set(lcov_options
+            --output-file coverage.info
+            --rc lcov_branch_coverage=1
+        )
         add_custom_command(TARGET ${coverage_target} POST_BUILD
-            COMMAND ${lcov_command} ${lcov_options} -c -d "${PROJECT_BINARY_DIR}" -b "${PROJECT_SOURCE_DIR}" --no-external
-            COMMAND ${lcov_command} ${lcov_options} -r coverage.info "*/external/*"
-            COMMAND ${lcov_command} ${lcov_options} -r coverage.info "*/tests/*"
-            COMMAND ${lcov_command} ${lcov_options} -r coverage.info "*_test.cpp"
-            COMMAND ${genhtml_command} -o coverage -t ${PROJECT_NAME} --branch-coverage --demangle-cpp coverage.info
+            COMMAND ${lcov_command} ${lcov_options}
+                --quiet
+                --capture
+                --directory "${PROJECT_BINARY_DIR}"
+                --base-directory "${PROJECT_SOURCE_DIR}"
+                --no-external
+            COMMAND ${lcov_command} ${lcov_options}
+                --quiet
+                --remove coverage.info
+                    "*/external/*"
+                    "*/tests/*"
+                    "*_test.cpp"
+            COMMAND ${lcov_command} ${lcov_options}
+                --list coverage.info
+            COMMAND ${lcov_command} ${lcov_options}
+                --summary coverage.info
+            COMMAND ${genhtml_command} coverage.info
+                --quiet
+                --output-directory coverage
+                --title ${PROJECT_NAME}
+                --branch-coverage
+                --demangle-cpp
+                --legend
             COMMENT "Generating ${${PROJECT_NAME}_coverage} report")
     endforeach()
 elseif(NOT ${PROJECT_NAME}_coverage STREQUAL "")
