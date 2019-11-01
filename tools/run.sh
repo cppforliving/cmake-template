0e006848f0e58ef5a81b43ab5f6ce37f9e22c5e5
cppforliving <cppforliving@gmail.com>
2019-11-01 13:32:51 +0100

    run.sh better types and locality

--- tools/run.sh
+++ tools/run.sh
@@ -1,183 +1,190 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-conan_config=Release
-cmake_config=Release
-cmake_shared=1
-valgrind=memcheck
-silenced=1
+main() {
+    declare conan_config=Release
+    declare cmake_config=Release
+    declare -i cmake_shared=1
+    declare valgrind=memcheck
+    declare -i silenced=1
 
-check=
-clean=0
-cmake_generator=
-cmake_toolchain=
-conan_update=
-coverage=
-doc=0
-examples=0
-format=0
-install=0
-memcheck=0
-pip_upgrade=
-rpaths=0
-sanitizer=
-stats=0
-testing=0
-vcpkg_upgrade=0
+    declare check=
+    declare -i clean=0
+    declare cmake_generator=
+    declare cmake_toolchain=
+    declare conan_update=
+    declare coverage=
+    declare -i doc=0
+    declare -i examples=0
+    declare -i format=0
+    declare -i install=0
+    declare -i memcheck=0
+    declare pip_upgrade=
+    declare -i rpaths=0
+    declare sanitizer=
+    declare -i stats=0
+    declare -i testing=0
+    declare -i vcpkg_upgrade=0
 
-for opt in "$@"; do
-    case $opt in
-    Conan)
-        readonly cmake_toolchain=conan_paths.cmake
+    declare opt
+    for opt in "$@"; do
+        case $opt in
+        Conan)
+            declare -r cmake_toolchain=conan_paths.cmake
+            ;;
+        Vcpkg)
+            declare -r cmake_toolchain=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
+            ;;
+        Clean)
+            declare -r clean=1
+            ;;
+        Format)
+            declare -r format=1
+            ;;
+        Static)
+            declare -r cmake_shared=0
+            ;;
+        Shared)
+            declare -r cmake_shared=1
+            ;;
+        Debug)
+            declare -r conan_config=Debug
+            declare -r cmake_config=$opt
+            ;;
+        Release | MinSizeRel | RelWithDebInfo)
+            declare -r conan_config=Release
+            declare -r cmake_config=$opt
+            ;;
+        Test)
+            declare -r testing=1
+            ;;
+        Coverage=*)
+            declare -r coverage=${opt#*=}
+            ;;
+        MemCheck=*)
+            declare -r memcheck=1
+            declare -r valgrind=${opt#*=}
+            ;;
+        Sanitizer=*)
+            declare -r sanitizer=${opt#*=}
+            ;;
+        Check=*)
+            declare -r check=${opt#*=}
+            ;;
+        Install)
+            declare -r install=1
+            ;;
+        Examples)
+            declare -r examples=1
+            ;;
+        Doc)
+            declare -r doc=1
+            ;;
+        Stats)
+            declare -r stats=1
+            ;;
+        Rpaths)
+            declare -r rpaths=1
+            ;;
+        Upgrade)
+            declare -r pip_upgrade=-U
+            declare -r conan_update=-u
+            declare -r vcpkg_upgrade=1
+            ;;
+        Verbose)
+            declare -r silenced=0
+            ;;
+        Ninja)
+            declare -r cmake_generator=-GNinja
+            ;;
+        *)
+            echo "unknown option '$opt'" >&2
+            exit 1
+            ;;
+        esac
+    done
+
+    silent() {
+        ((silenced)) || set +x
+        "$@"
+        ((silenced)) || set -x
+    }
+    readonly -f silent
+
+    source_if_exists() {
+        [[ ! -f $1 ]] || source "$@"
+    }
+    readonly -f source_if_exists
+
+    ((silenced)) || set -x
+
+    declare -r build_dir=./build/$cmake_config
+    ((clean)) && rm -r "$build_dir"
+    mkdir -p "$build_dir"
+
+    declare -r venv_dir=./build/venv
+    [[ ! -d $venv_dir || $pip_upgrade ]] && python -m virtualenv "$venv_dir"
+    silent source "$venv_dir"/bin/activate
+
+    pip install $pip_upgrade -r requirements-dev.txt
+
+    case $(basename "$cmake_toolchain") in
+    conan_paths.cmake)
+        pip install $pip_upgrade conan
+        conan profile new "$build_dir"/conan/detected --detect --force
+        conan profile update settings.compiler.libcxx=libstdc++11 "$build_dir"/conan/detected
+        conan install . $conan_update \
+            -if "$build_dir" \
+            -s build_type="$conan_config" \
+            -pr "$build_dir"/conan/detected \
+            -b missing
         ;;
-    Vcpkg)
-        readonly cmake_toolchain=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
-        ;;
-    Clean)
-        readonly clean=1
-        ;;
-    Format)
-        readonly format=1
-        ;;
-    Static)
-        readonly cmake_shared=0
-        ;;
-    Shared)
-        readonly cmake_shared=1
-        ;;
-    Debug)
-        readonly conan_config=Debug
-        readonly cmake_config=$opt
-        ;;
-    Release | MinSizeRel | RelWithDebInfo)
-        readonly conan_config=Release
-        readonly cmake_config=$opt
-        ;;
-    Test)
-        readonly testing=1
-        ;;
-    Coverage=*)
-        readonly coverage=${opt#*=}
-        ;;
-    MemCheck=*)
-        readonly memcheck=1
-        readonly valgrind=${opt#*=}
-        ;;
-    Sanitizer=*)
-        readonly sanitizer=${opt#*=}
-        ;;
-    Check=*)
-        readonly check=${opt#*=}
-        ;;
-    Install)
-        readonly install=1
-        ;;
-    Examples)
-        readonly examples=1
-        ;;
-    Doc)
-        readonly doc=1
-        ;;
-    Stats)
-        readonly stats=1
-        ;;
-    Rpaths)
-        readonly rpaths=1
-        ;;
-    Upgrade)
-        readonly pip_upgrade=-U
-        readonly conan_update=-u
-        readonly vcpkg_upgrade=1
-        ;;
-    Verbose)
-        readonly silenced=0
-        ;;
-    Ninja)
-        readonly cmake_generator=-GNinja
-        ;;
-    *)
-        echo "unknown option '$opt'" >&2
-        exit 1
+    vcpkg.cmake)
+        if ((vcpkg_upgrade)); then
+            "$VCPKG_ROOT"/vcpkg update
+            "$VCPKG_ROOT"/vcpkg upgrade --no-dry-run
+        fi
+        "$VCPKG_ROOT"/vcpkg install @vcpkgfile.txt
         ;;
     esac
-done
 
-silent() {
-    ((silenced)) || set +x
-    "$@"
-    ((silenced)) || set -x
-}
-readonly -f silent
+    cmake . \
+        -B"$build_dir" \
+        "$cmake_generator" \
+        -DBUILD_SHARED_LIBS="$cmake_shared" \
+        -DBUILD_TESTING="$testing" \
+        -DBUILD_EXAMPLES="$examples" \
+        -DBUILD_DOCS="$doc" \
+        -DCMAKE_BUILD_TYPE="$cmake_config" \
+        -DCMAKE_TOOLCHAIN_FILE="$cmake_toolchain" \
+        -Ddebug_dynamic_deps="$rpaths" \
+        -Dprojname_coverage="$coverage" \
+        -Dprojname_valgrind="$valgrind" \
+        -Dprojname_sanitizer="$sanitizer" \
+        -Dprojname_check="$check"
 
-source_if_exists() {
-    [[ ! -f $1 ]] || source "$@"
-}
-readonly -f source_if_exists
+    declare -r make_cmd="cmake --build $build_dir -j $(nproc) --"
 
-((silenced)) || set -x
-
-readonly build_dir=./build/$cmake_config
-((clean)) && rm -rf "$build_dir"
-mkdir -p "$build_dir"
-
-readonly venv_dir=./build/venv
-[[ ! -d $venv_dir || $pip_upgrade ]] && python -m virtualenv "$venv_dir"
-silent source "$venv_dir"/bin/activate
-
-pip install $pip_upgrade -r requirements-dev.txt
-
-case $(basename "$cmake_toolchain") in
-conan_paths.cmake)
-    pip install $pip_upgrade conan
-    conan profile new "$build_dir"/conan/detected --detect --force
-    conan profile update settings.compiler.libcxx=libstdc++11 "$build_dir"/conan/detected
-    conan install . $conan_update \
-        -if "$build_dir" \
-        -s build_type="$conan_config" \
-        -pr "$build_dir"/conan/detected \
-        -b missing
-    ;;
-vcpkg.cmake)
-    if ((vcpkg_upgrade)); then
-        "$VCPKG_ROOT"/vcpkg update
-        "$VCPKG_ROOT"/vcpkg upgrade --no-dry-run
+    ((stats)) && ccache -z
+    ((format)) && $make_cmd format
+    $make_cmd all
+    if ((testing)); then
+        silent source_if_exists "$build_dir"/activate_run.sh
+        if ((memcheck)); then
+            CTEST_OUTPUT_ON_FAILURE=1 $make_cmd ExperimentalMemCheck
+        else
+            CTEST_OUTPUT_ON_FAILURE=1 $make_cmd ExperimentalTest
+        fi
+        silent source_if_exists "$build_dir"/deactivate_run.sh
     fi
-    "$VCPKG_ROOT"/vcpkg install @vcpkgfile.txt
-    ;;
-esac
+    [[ $coverage ]] && CTEST_OUTPUT_ON_FAILURE=1 $make_cmd ExperimentalCoverage
+    ((doc)) && $make_cmd doc
+    ((install)) && $make_cmd install
+    ((stats)) && ccache -s
 
-cmake . \
-    -B"$build_dir" \
-    "$cmake_generator" \
-    -DBUILD_SHARED_LIBS="$cmake_shared" \
-    -DBUILD_TESTING="$testing" \
-    -DBUILD_EXAMPLES="$examples" \
-    -DBUILD_DOCS="$doc" \
-    -DCMAKE_BUILD_TYPE="$cmake_config" \
-    -DCMAKE_TOOLCHAIN_FILE="$cmake_toolchain" \
-    -Ddebug_dynamic_deps="$rpaths" \
-    -Dprojname_coverage="$coverage" \
-    -Dprojname_valgrind="$valgrind" \
-    -Dprojname_sanitizer="$sanitizer" \
-    -Dprojname_check="$check"
+    silent deactivate
+}
 
-readonly make_cmd="cmake --build $build_dir -j $(nproc) --"
-
-((stats)) && ccache -z
-((format)) && $make_cmd format
-$make_cmd all
-if ((testing)); then
-    silent source_if_exists "$build_dir"/activate_run.sh
-    if ((memcheck)); then
-        CTEST_OUTPUT_ON_FAILURE=1 $make_cmd ExperimentalMemCheck
-    else
-        CTEST_OUTPUT_ON_FAILURE=1 $make_cmd ExperimentalTest
-    fi
-    silent source_if_exists "$build_dir"/deactivate_run.sh
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+    main "$@"
 fi
-[[ $coverage ]] && CTEST_OUTPUT_ON_FAILURE=1 $make_cmd ExperimentalCoverage
-((doc)) && $make_cmd doc
-((install)) && $make_cmd install
-((stats)) && ccache -s
-
-silent deactivate
