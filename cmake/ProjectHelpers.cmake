65cacff82cb77ae639fa7da4bae0c50670140d41
cppforliving <cppforliving@gmail.com>
2019-12-16 23:50:45 +0100

    handle windows absolute paths and minor improvements

--- cmake/ProjectHelpers.cmake
+++ cmake/ProjectHelpers.cmake
@@ -27,19 +27,19 @@ function(add_custom_library lib_name)
     set(options INTERFACE)
     set(one_value_args)
     set(multi_value_args SOURCES DEPENDS)
-    cmake_parse_arguments(${lib_name} "${options}"
+    cmake_parse_arguments(lib "${options}"
         "${one_value_args}" "${multi_value_args}" ${ARGN})
 
     set(header_regex ".*\\.h(h|pp|xx|\\+\\+)?$")
-    list(TRANSFORM ${lib_name}_SOURCES
+    list(TRANSFORM lib_SOURCES
         PREPEND "${CMAKE_CURRENT_SOURCE_DIR}/"
-        REGEX "^[^\/].*"
+        REGEX "^[^\/][^:].*"
     )
-    set(${lib_name}_HEADERS ${${lib_name}_SOURCES})
-    list(FILTER ${lib_name}_SOURCES EXCLUDE REGEX ${header_regex})
-    list(FILTER ${lib_name}_HEADERS INCLUDE REGEX ${header_regex})
+    set(lib_HEADERS ${lib_SOURCES})
+    list(FILTER lib_SOURCES EXCLUDE REGEX ${header_regex})
+    list(FILTER lib_HEADERS INCLUDE REGEX ${header_regex})
 
-    if(${lib_name}_INTERFACE)
+    if(lib_INTERFACE)
         set(lib_type INTERFACE)
         set(visibility INTERFACE)
     else()
@@ -48,12 +48,12 @@ function(add_custom_library lib_name)
 
     add_library(${lib_name} ${lib_type})
     add_library(${PROJECT_NAME}::${lib_name} ALIAS ${lib_name})
-    if(NOT ${lib_name}_INTERFACE)
+    if(NOT lib_INTERFACE)
         generate_export_header(${lib_name}
             EXPORT_FILE_NAME export.h
             DEFINE_NO_DEPRECATED
         )
-        list(APPEND ${lib_name}_HEADERS "${CMAKE_CURRENT_BINARY_DIR}/export.h")
+        list(APPEND lib_HEADERS "${CMAKE_CURRENT_BINARY_DIR}/export.h")
         set_target_properties(${lib_name}
           PROPERTIES
             SOVERSION ${PROJECT_VERSION_MAJOR}
@@ -67,12 +67,12 @@ function(add_custom_library lib_name)
         endif()
         target_sources(${lib_name}
           PRIVATE
-            ${${lib_name}_SOURCES}
+            ${lib_SOURCES}
         )
     endif()
     target_sources(${lib_name}
       ${visibility}
-        "$<BUILD_INTERFACE:${${lib_name}_HEADERS}>"
+        "$<BUILD_INTERFACE:${lib_HEADERS}>"
     )
     get_filename_component(parent_source_dir "${CMAKE_CURRENT_SOURCE_DIR}" DIRECTORY)
     get_filename_component(parent_binary_dir "${CMAKE_CURRENT_BINARY_DIR}" DIRECTORY)
@@ -87,9 +87,9 @@ function(add_custom_library lib_name)
     )
     target_link_libraries(${lib_name}
       ${visibility}
-        ${${lib_name}_DEPENDS}
+        ${lib_DEPENDS}
     )
-    if(NOT ${lib_name}_INTERFACE)
+    if(NOT lib_INTERFACE)
         debug_dynamic_dependencies(${lib_name})
     endif()
 
@@ -109,53 +109,52 @@ function(add_custom_library lib_name)
     # install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/"
     #     DESTINATION "${install_prefix}${CMAKE_INSTALL_INCLUDEDIR}/${lib_name}"
     #     FILES_MATCHING REGEX ${header_regex})
-    install(FILES ${${lib_name}_HEADERS}
+    install(FILES ${lib_HEADERS}
         DESTINATION "${install_prefix}${CMAKE_INSTALL_INCLUDEDIR}/${lib_name}")
 endfunction()
 
-function(add_custom_executable exe_name)
+function(add_custom_executable exec_name)
     set(options)
     set(one_value_args)
     set(multi_value_args SOURCES DEPENDS)
-    cmake_parse_arguments(${exe_name} "${options}"
+    cmake_parse_arguments(exec "${options}"
         "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-    add_executable(${exe_name}_app)
-    add_executable(${PROJECT_NAME}::${exe_name}_app ALIAS ${exe_name}_app)
-    set_target_properties(${exe_name}_app
+    add_executable(${exec_name})
+    add_executable(${PROJECT_NAME}::${exec_name} ALIAS ${exec_name})
+    set_target_properties(${exec_name}
       PROPERTIES
-        OUTPUT_NAME ${exe_name}
         VERSION ${PROJECT_VERSION}
     )
     if(UNIX AND NOT APPLE)
-        set_target_properties(${exe_name}_app
+        set_target_properties(${exec_name}
           PROPERTIES
             INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}"
         )
     endif()
-    target_sources(${exe_name}_app
+    target_sources(${exec_name}
       PRIVATE
-        ${${exe_name}_SOURCES}
+        ${exec_SOURCES}
     )
     get_filename_component(parent_source_dir "${CMAKE_CURRENT_SOURCE_DIR}" DIRECTORY)
     get_filename_component(parent_binary_dir "${CMAKE_CURRENT_BINARY_DIR}" DIRECTORY)
-    target_include_directories(${exe_name}_app
+    target_include_directories(${exec_name}
       PUBLIC
         $<BUILD_INTERFACE:${parent_source_dir}>
         $<BUILD_INTERFACE:${parent_binary_dir}>
         $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
     )
-    target_link_libraries(${exe_name}_app
+    target_link_libraries(${exec_name}
       PUBLIC
-        ${${exe_name}_DEPENDS}
+        ${exec_DEPENDS}
     )
-    debug_dynamic_dependencies(${exe_name}_app)
+    debug_dynamic_dependencies(${exec_name})
 
     get_filename_component(parent_source_dir_name "${parent_source_dir}" NAME)
     if("${parent_source_dir_name}" STREQUAL "examples")
         set(install_prefix "${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}/examples/")
     endif()
-    install(TARGETS ${exe_name}_app
+    install(TARGETS ${exec_name}
         EXPORT ${PROJECT_NAME}-targets
         RUNTIME DESTINATION "${install_prefix}${CMAKE_INSTALL_BINDIR}"
                 COMPONENT ${PROJECT_NAME}_Runtime)
@@ -165,20 +164,20 @@ function(add_custom_test test_name)
     set(options)
     set(one_value_args)
     set(multi_value_args SOURCES DEPENDS EXTRA_ARGS)
-    cmake_parse_arguments(${test_name} "${options}"
+    cmake_parse_arguments(test "${options}"
         "${one_value_args}" "${multi_value_args}" ${ARGN})
 
     add_executable(${test_name})
     target_sources(${test_name}
       PRIVATE
-        ${${test_name}_SOURCES}
+        ${test_SOURCES}
     )
     target_link_libraries(${test_name}
       PUBLIC
-        ${${test_name}_DEPENDS}
+        ${test_DEPENDS}
     )
     debug_dynamic_dependencies(${test_name})
     add_test(NAME ${test_name} COMMAND ${test_name}
-        ${${test_name}_EXTRA_ARGS}
+        ${test_EXTRA_ARGS}
     )
 endfunction()
