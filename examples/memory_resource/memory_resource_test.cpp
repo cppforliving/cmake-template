575b41b77fbee52b32b9a8cc1c2b9f76ead776fe
cppforliving <cppforliving@gmail.com>
2020-02-02 00:53:18 +0100

    memory_resource for osx

--- examples/memory_resource/memory_resource_test.cpp
+++ examples/memory_resource/memory_resource_test.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
+#if __has_include(<memory_resource>)
 #include <memory_resource>
+namespace pmr = std::pmr;
+#else
+#include <experimental/memory_resource>
+namespace pmr = std::experimental::pmr;
+#endif
 
 namespace memory_resource {
 namespace {
@@ -7,30 +13,30 @@ namespace {
 using Simple = int;
 
 struct Complex {
-    std::pmr::vector<Simple> simple_list;
+    pmr::vector<Simple> simple_list;
 };
 
 struct Producer {
-    std::pmr::vector<Complex> produce() {
-        return std::pmr::vector<Complex>{1000, std::pmr::new_delete_resource()};
+    pmr::vector<Complex> produce() {
+        return pmr::vector<Complex>{1000, pmr::new_delete_resource()};
     }
 };
 
 struct Consumer {
     void consume() { complex_list = producer.produce(); }
     Producer& producer;
-    std::pmr::vector<Complex> complex_list{};
+    pmr::vector<Complex> complex_list{};
 };
 
 TEST(memory_resource, default_null_memory_resource) {
-    std::pmr::set_default_resource(std::pmr::null_memory_resource());
+    pmr::set_default_resource(pmr::null_memory_resource());
     Producer producer;
     Consumer consumer{producer};
     EXPECT_THROW(consumer.consume(), std::bad_alloc);
 }
 
 TEST(memory_resource, default_new_delete_resource) {
-    std::pmr::set_default_resource(std::pmr::new_delete_resource());
+    pmr::set_default_resource(pmr::new_delete_resource());
     Producer producer;
     Consumer consumer{producer};
     EXPECT_NO_THROW(consumer.consume());
