9fe41bbb7c3a7a193b3421aad798400be9cda3b1
cppforliving <cppforliving@gmail.com>
2020-04-13 14:17:33 +0200

    cmade improve scoping and include guards

--- cmake/cmade/EnableDiagnosticsColor.cmake
+++ cmake/cmade/EnableDiagnosticsColor.cmake
@@ -1,4 +1,4 @@
-include_guard(DIRECTORY)
+include_guard(GLOBAL)
 
 
 function(_cmade_enable_diagnostics_color)
--- cmake/cmade/InstallPackage.cmake
+++ cmake/cmade/InstallPackage.cmake
@@ -1,28 +1,33 @@
-include_guard(DIRECTORY)
+include_guard(GLOBAL)
 
 include(GNUInstallDirs)
 include(CMakePackageConfigHelpers)
 
 
-write_basic_package_version_file(
-    "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config-version.cmake"
-    VERSION ${PROJECT_VERSION}
-    COMPATIBILITY SameMajorVersion
-)
-configure_package_config_file(
-    "${CMAKE_CURRENT_LIST_DIR}/package-config.cmake.in"
-    "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config.cmake"
-    INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
-    NO_SET_AND_CHECK_MACRO
-    NO_CHECK_REQUIRED_COMPONENTS_MACRO
-)
+function(_cmade_install_package)
+    write_basic_package_version_file(
+        "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config-version.cmake"
+        VERSION ${PROJECT_VERSION}
+        COMPATIBILITY SameMajorVersion
+    )
+    configure_package_config_file(
+        "${CMAKE_CURRENT_LIST_DIR}/package-config.cmake.in"
+        "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config.cmake"
+        INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+        NO_SET_AND_CHECK_MACRO
+        NO_CHECK_REQUIRED_COMPONENTS_MACRO
+    )
 
-install(EXPORT ${PROJECT_NAME}-targets
-    NAMESPACE ${PROJECT_NAME}::
-    DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
-)
-install(FILES
-    "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config.cmake"
-    "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config-version.cmake"
-    DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
-)
+    install(EXPORT ${PROJECT_NAME}-targets
+        NAMESPACE ${PROJECT_NAME}::
+        DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+    )
+    install(FILES
+        "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config.cmake"
+        "${PROJECT_BINARY_DIR}/cmake/${PROJECT_NAME}-config-version.cmake"
+        DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+    )
+endfunction()
+
+
+_cmade_install_package()
