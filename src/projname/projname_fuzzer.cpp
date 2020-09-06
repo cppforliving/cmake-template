fbfa4c15b1cdb6a82cf186e58b6ed3335b52c832
cppforliving <cppforliving@gmail.com>
2020-09-06 19:03:49 +0200

    [skip appveyor] fix clang-tidy

--- src/projname/projname_fuzzer.cpp
+++ src/projname/projname_fuzzer.cpp
@@ -10,21 +10,22 @@ extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* data, std::size_t size
 
 int LLVMFuzzerTestOneInput(std::uint8_t const* data, std::size_t const size) {
     absl::Span<std::uint8_t const> const data_span{data, size};
-    std::vector<char> cdata;
-    cdata.reserve(size + 1);
-    for (auto& byte : data_span) {
-        cdata.push_back(absl::bit_cast<char>(byte));
+
+    std::vector<char> zdata;
+    zdata.reserve(size + 1);
+    for (auto const& byte : data_span) {
+        zdata.push_back(absl::bit_cast<char>(byte));
     }
-    cdata.push_back('\0');
-    spdlog::info("{} size: {} data: '{}'", __func__, size, cdata.data());
+    zdata.push_back('\0');
 
     std::vector<char const*> args;
-    args.push_back(cdata.data());
-    for (std::size_t i = 1; i < cdata.size(); ++i) {
-        if (cdata[i] == '\0') {
-            args.push_back(&cdata[i - 1]);
+    args.push_back(zdata.data());
+    for (std::size_t i = 1; i < zdata.size(); ++i) {
+        if (zdata[i] == '\0') {
+            args.push_back(&zdata[i - 1]);
         }
     }
+
     projname::run(static_cast<int>(args.size()), args.data());
     return 0;
 }
